e36c8a5e3288aa9532d59336bdd24049a5c53cdf
BogdanRaven <55992260+BogdanRaven@users.noreply.github.com>
2020-05-22 15:37:47 +0300

    Add files via upload
    Old project

---
+++ ProgPuzl/ProgPuzl/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace ProgPuzl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограмеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.картинкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.гитлерToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.мопсToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.шрекToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.гомерToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.futuramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gravityFallsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.артЗГаріПотераToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.witherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.поставитьНомеркиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labeltimermin = new System.Windows.Forms.Label();
+            this.labeltimersec = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.оПрограмеToolStripMenuItem,
+            this.картинкаToolStripMenuItem,
+            this.поставитьНомеркиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem1.ForeColor = System.Drawing.Color.Blue;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(92, 20);
+            this.toolStripMenuItem1.Text = "Новая игра";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // оПрограмеToolStripMenuItem
+            // 
+            this.оПрограмеToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.оПрограмеToolStripMenuItem.Name = "оПрограмеToolStripMenuItem";
+            this.оПрограмеToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.оПрограмеToolStripMenuItem.Text = "О програме";
+            this.оПрограмеToolStripMenuItem.Click += new System.EventHandler(this.оПрограмеToolStripMenuItem_Click_1);
+            // 
+            // картинкаToolStripMenuItem
+            // 
+            this.картинкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.гитлерToolStripMenuItem,
+            this.мопсToolStripMenuItem,
+            this.шрекToolStripMenuItem,
+            this.гомерToolStripMenuItem,
+            this.futuramaToolStripMenuItem,
+            this.gravityFallsToolStripMenuItem,
+            this.артЗГаріПотераToolStripMenuItem,
+            this.witherToolStripMenuItem});
+            this.картинкаToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.картинкаToolStripMenuItem.Name = "картинкаToolStripMenuItem";
+            this.картинкаToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.картинкаToolStripMenuItem.Text = "Картинка";
+            // 
+            // гитлерToolStripMenuItem
+            // 
+            this.гитлерToolStripMenuItem.Name = "гитлерToolStripMenuItem";
+            this.гитлерToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.гитлерToolStripMenuItem.Text = "Пикачу";
+            this.гитлерToolStripMenuItem.Click += new System.EventHandler(this.ПикачуToolStripMenuItem_Click);
+            // 
+            // мопсToolStripMenuItem
+            // 
+            this.мопсToolStripMenuItem.Name = "мопсToolStripMenuItem";
+            this.мопсToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.мопсToolStripMenuItem.Text = "PewDiPy";
+            this.мопсToolStripMenuItem.Click += new System.EventHandler(this.мопсToolStripMenuItem_Click);
+            // 
+            // шрекToolStripMenuItem
+            // 
+            this.шрекToolStripMenuItem.Name = "шрекToolStripMenuItem";
+            this.шрекToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.шрекToolStripMenuItem.Text = "Шрек";
+            this.шрекToolStripMenuItem.Click += new System.EventHandler(this.шрекToolStripMenuItem_Click);
+            // 
+            // гомерToolStripMenuItem
+            // 
+            this.гомерToolStripMenuItem.Name = "гомерToolStripMenuItem";
+            this.гомерToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.гомерToolStripMenuItem.Text = "Гомер";
+            this.гомерToolStripMenuItem.Click += new System.EventHandler(this.гомерToolStripMenuItem_Click);
+            // 
+            // futuramaToolStripMenuItem
+            // 
+            this.futuramaToolStripMenuItem.Name = "futuramaToolStripMenuItem";
+            this.futuramaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.futuramaToolStripMenuItem.Text = "Futurama";
+            this.futuramaToolStripMenuItem.Click += new System.EventHandler(this.futuramaToolStripMenuItem_Click);
+            // 
+            // gravityFallsToolStripMenuItem
+            // 
+            this.gravityFallsToolStripMenuItem.Name = "gravityFallsToolStripMenuItem";
+            this.gravityFallsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gravityFallsToolStripMenuItem.Text = "gravity falls";
+            this.gravityFallsToolStripMenuItem.Click += new System.EventHandler(this.gravityFallsToolStripMenuItem_Click);
+            // 
+            // артЗГаріПотераToolStripMenuItem
+            // 
+            this.артЗГаріПотераToolStripMenuItem.Name = "артЗГаріПотераToolStripMenuItem";
+            this.артЗГаріПотераToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.артЗГаріПотераToolStripMenuItem.Text = "Арт з Гарі Потера";
+            this.артЗГаріПотераToolStripMenuItem.Click += new System.EventHandler(this.артЗГаріПотераToolStripMenuItem_Click);
+            // 
+            // witherToolStripMenuItem
+            // 
+            this.witherToolStripMenuItem.Name = "witherToolStripMenuItem";
+            this.witherToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.witherToolStripMenuItem.Text = "Wither";
+            this.witherToolStripMenuItem.Click += new System.EventHandler(this.witherToolStripMenuItem_Click);
+            // 
+            // поставитьНомеркиToolStripMenuItem
+            // 
+            this.поставитьНомеркиToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.поставитьНомеркиToolStripMenuItem.Name = "поставитьНомеркиToolStripMenuItem";
+            this.поставитьНомеркиToolStripMenuItem.Size = new System.Drawing.Size(128, 20);
+            this.поставитьНомеркиToolStripMenuItem.Text = "Поставить номерки";
+            this.поставитьНомеркиToolStripMenuItem.Click += new System.EventHandler(this.поставитьНомеркиToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labeltimermin
+            // 
+            this.labeltimermin.AutoSize = true;
+            this.labeltimermin.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labeltimermin.Location = new System.Drawing.Point(382, 3);
+            this.labeltimermin.Name = "labeltimermin";
+            this.labeltimermin.Size = new System.Drawing.Size(13, 15);
+            this.labeltimermin.TabIndex = 1;
+            this.labeltimermin.Text = "0";
+            // 
+            // labeltimersec
+            // 
+            this.labeltimersec.AutoSize = true;
+            this.labeltimersec.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labeltimersec.Location = new System.Drawing.Point(417, 3);
+            this.labeltimersec.Name = "labeltimersec";
+            this.labeltimersec.Size = new System.Drawing.Size(13, 15);
+            this.labeltimersec.TabIndex = 2;
+            this.labeltimersec.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(401, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = ":";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labeltimersec);
+            this.Controls.Add(this.labeltimermin);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick_1);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограмеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem картинкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem гитлерToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem мопсToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem шрекToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem поставитьНомеркиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem гомерToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem futuramaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gravityFallsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem артЗГаріПотераToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem witherToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labeltimermin;
+        private System.Windows.Forms.Label labeltimersec;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProgPuzl/ProgPuzl/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+
+using System.Drawing;
+using WMPLib;
+using System.Windows.Forms;
+
+namespace ProgPuzl
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        // 4х4 - размер игрового поля
+        const int nw = 4, nh = 4;
+
+        // графическая поверхность формы
+        Graphics g;
+
+        // картинка
+        Bitmap pics;
+
+        // размер (ширина и высота) клетки
+        int cw, ch;
+
+        // игровое поле: хранит номера фрагментов
+        // картинки
+        int[,] field = new int[nw, nh];
+
+        // координаты пустой клетки
+        int ex, ey;
+
+        int timersec = 0, timerm=0;
+
+        //  отображения номера фишки
+        Boolean showNumb=false;
+        public Form1()
+        {
+            InitializeComponent();
+           
+            try
+            {
+                wplayer.URL = "res/muzikfon.mp3";
+                wplayer.settings.setMode("loop", true);
+                // загружаем файл картинки
+                pics = new Bitmap("res/picture.bmp");
+                pics = new Bitmap("res/Shrek.bmp");
+                pics = new Bitmap("res/Mops.bmp");
+                pics = new Bitmap("res/Gomer.bmp");
+                pics = new Bitmap("res/Gravity.bmp");
+                pics = new Bitmap("res/Futurama.bmp");
+                pics = new Bitmap("res/Wither.bmp");
+                pics = new Bitmap("res/Garri.bmp");
+              
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Все файлы не присутствуют.\n",
+                    "Собери картинку",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                Close();
+                return;
+            }
+            optionpics();
+           
+            newGame();
+            timer1.Stop();
+        }
+        //устанавливаем значения
+        public void optionpics()
+        {
+            cw = (int)(pics.Width / nw);
+            ch = (int)(pics.Height / nh);
+
+            // установить размер клиентской (внутренней) области формы
+            ClientSize =
+                new Size(cw * nw + 1, ch * nh + 1 + menuStrip1.Height);
+            // рабочая графическая поверхность
+            g = CreateGraphics();
+        }
+        //новая игра
+        private void newGame()
+        { // располагаем фишки в правильном порядке
+         
+            for (int j = 0; j < nh; j++)
+                for (int i = 0; i < nw; i++)
+                    field[i, j] = j * nw + i + 1;
+
+            // последняя фишка - пустая
+            field[nw - 1, nh - 1] = 0;
+            ex = nw - 1; ey = nh - 1;
+
+            mixer();        // перемешиваем фишки
+            drawField();    // выводим игровое поле
+            timersec = 0;
+            timerm = 0;
+            labeltimermin.Text = "0";
+            timer1.Start();
+
+        }
+        // перемешиваем фишки
+        private void mixer()
+        {
+            int d;    // положение (относительно пустой) перемещаемой
+                      // клетки: 0 - слева; 1 - справа; 2 - сверху; 3 - снизу.
+
+            int x, y; // координаты клеткы
+
+            // генератор случайных чисел
+            Random rnd = new Random();
+
+            for (int i = 0; i < nw * nh * 10; i++)
+            // nw * nh * 10 - кол-во перестановок
+            {
+                x = ex;
+                y = ey;
+
+                d = rnd.Next(4);
+                switch (d)
+                {
+                    case 0: if (x > 0) x--; break;
+                    case 1: if (x < nw - 1) x++; break;
+                    case 2: if (y > 0) y--; break;
+                    case 3: if (y < nh - 1) y++; break;
+                }
+                // здесь определили фишку, которую
+                // нужно переместить в пустую клетку
+                field[ex, ey] = field[x, y];
+                field[x, y] = 0;
+
+                // запоминаем координаты пустой фишки
+                ex = x; ey = y;
+            }
+        }
+        // отрисовывает поле
+        private void drawField()
+        {
+            // содержимое клеток
+            for (int i = 0; i < nw; i++)
+                for (int j = 0; j < nh; j++)
+                {
+                    if (field[i, j] != 0)
+                        // выводим фишку с картинкой:
+                        
+                        // координаты левого верхнего угла
+                        // области файла-источника картинки
+                        g.DrawImage(pics,
+                            new Rectangle(i * cw, j * ch + menuStrip1.Height, cw, ch),
+                            new Rectangle(
+                                ((field[i, j] - 1) % nw) * cw,
+                                ((field[i, j] - 1) / nw) * ch,
+                                cw, ch),
+                            GraphicsUnit.Pixel);
+                    else
+                        // выводим пустую фишку
+                        g.FillRectangle(SystemBrushes.Control,
+                            i * cw, j * ch + menuStrip1.Height, cw, ch);
+                    // рисуем границу
+                    g.DrawRectangle(Pens.Black,
+                        i * cw, j * ch + menuStrip1.Height, cw, ch);
+
+                    // номер фишки
+                    if ((showNumb) && field[i, j] != 0)
+                        g.DrawString(Convert.ToString(field[i, j]),
+                            new Font("Tahoma", 10, FontStyle.Bold),
+                            Brushes.Black, i * cw + 5, j * ch + 5 + menuStrip1.Height);
+                }
+        }
+        // проверяет, расположены ли фишки в правильном порядке
+        private Boolean finish()
+        {
+            // координаты клетки
+            int i = 0;
+            int j = 0;
+
+            int c;       // число в клетке
+
+            // фишки расположены правильно, если
+            
+            //   1  2  3  4
+            //   5  6  7  8
+            //   9 10 11 12
+            //  13 14 15
+
+            for (c = 1; c < nw * nh; c++)
+            {
+                if (field[i, j] != c) return false;
+
+                // к следующей клетке
+                if (i < nw - 1) i++;
+                else { i = 0; j++; }
+            }
+            timer1.Stop();
+            return true;
+        }
+        // перемещает фишку, на которой сделан щелчок,
+    
+        private void move(int cx, int cy)
+        {
+            // проверим, возможен ли обмен
+            if (!(((Math.Abs(cx - ex) == 1) && (cy - ey == 0)) ||
+                ((Math.Abs(cy - ey) == 1) && (cx - ex == 0))))
+                return;
+            // обмен. переместим фишку из (x, y) в (ex, ey)
+            field[ex, ey] = field[cx, cy];
+            field[cx, cy] = 0;
+
+            ex = cx; ey = cy;
+
+            // отрисовать поле
+            drawField();
+
+            if (finish())
+            {
+                field[nw - 1, nh - 1] = nh * nw;
+                drawField();
+
+               
+                if (MessageBox.Show("Поздравляю! Ви собрали картинку !\n Ваше время:"+timerm+"м  "+timersec+"с\n" +
+                         "Еще раз?", "ProgPuzl",
+                          MessageBoxButtons.YesNo,
+                          MessageBoxIcon.Question)
+                          == DialogResult.No)
+                    Close();
+                else newGame();
+            }
+        }
+        // обработка события Paint
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            drawField();
+
+        }
+
+        private void ПикачуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pics = new Bitmap("res/picture.bmp");
+            optionpics();
+            newGame();
+        }
+
+        private void шрекToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pics = new Bitmap("res/Shrek.bmp");
+            optionpics();
+            newGame();
+        }
+
+        private void мопсToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pics = new Bitmap("res/Mops.bmp");
+            optionpics();
+            newGame();
+        }
+
+       
+
+        private void поставитьНомеркиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ( поставитьНомеркиToolStripMenuItem.Text == "Поставить номерки")
+            {
+                showNumb = true;
+                поставитьНомеркиToolStripMenuItem.Text = "Убрать номера";
+                drawField();
+                
+            }
+           else if(поставитьНомеркиToolStripMenuItem.Text == "Убрать номера")
+            {
+                showNumb = false;
+                поставитьНомеркиToolStripMenuItem.Text = "Поставить номерки";
+                drawField();
+            }
+            timer1.Enabled = true;
+        }
+
+        private void гомерToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pics = new Bitmap("res/Gomer.bmp");
+            optionpics();
+            newGame();
+        }
+
+        private void артЗГаріПотераToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pics = new Bitmap("res/Garri.bmp");
+            optionpics();
+            newGame();
+        }
+
+        private void gravityFallsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pics = new Bitmap("res/Gravity.bmp");
+            optionpics();
+            newGame();
+        }
+
+        private void futuramaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pics = new Bitmap("res/Futurama.bmp");
+            optionpics();
+            newGame();
+        }
+
+        private void witherToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pics = new Bitmap("res/Wither.bmp");
+            optionpics();
+            newGame();
+        }
+
+    
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData==Keys.Escape)
+            {
+                if (MessageBox.Show("Вы точно хотите выйти из самой лучшй игры на планете? \n" 
+                          , "????????????",
+                           MessageBoxButtons.YesNo,
+                           MessageBoxIcon.Question)
+                           == DialogResult.Yes)
+                    Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            timersec++;
+            if (timersec == 60)
+            {
+                timerm++;
+                labeltimermin.Text = "" + timerm+" м";
+                timersec = 0;
+            }
+            labeltimersec.Text = "" + timersec+" c";
+        }
+
+        private void Form1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            // щелчок кнопкой мыши на игровом поле
+            // преобразуем координаты мыши в координаты клетки
+            move(e.X / cw, (e.Y - menuStrip1.Height) / ch);
+        }
+
+        private void оПрограмеToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Прога v1.0 \n Автор Богдан\n  Спасиба что играете игру",
+                      "ProgPuzl",
+                      MessageBoxButtons.OK,
+                      MessageBoxIcon.Information);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            newGame();
+            timer1.Enabled = true;
+        }
+    }
+}
